0bf291efab3b1401b7a81261b5673708d1b9aa32
Jordan Lenard David <jordan_david02@hotmail.com>
2023-10-09 18:05:44 -0400

    [u] Updated summary

--- src/components/Summary.js
+++ src/components/Summary.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 function Summary() {
-    const Description = "Hi I'm Jordan, I am a software developer from the Philippines. I am passionate in learning and experiencing new things. I've handled projects ranging from embedded programming of wireless technologies to full stack web development. I am versatile and adaptable, capable of doing what needs to be done. Outside of work, I love to sit at home and relax but I also enjoy travelling to experience the joys that the world has to offer."
+    const Description = "Hi I'm Jordan, I am a software developer with a decade of experience. I am passionate in learning and experiencing new things. I've handled projects ranging from embedded programming of wireless technologies to full stack web development. I am versatile and adaptable, capable of doing what needs to be done. Outside of work, I love to sit at home and relax with a cup of coffee but I also enjoy travelling to experience the joys that the world has to offer."
     return <div><h3 className='Summary'>{Description}</h3></div>
 }
 
